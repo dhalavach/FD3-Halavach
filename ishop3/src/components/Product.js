a22e2902b408d6faa2d24e42d727827ecc9af329
Dzmitry Halavach <kotik1@protonmail.com>
2023-02-27 00:41:45 +0300

    refactor: function to class components, select and remove not working yet

--- ishop3/src/components/Product.js
+++ ishop3/src/components/Product.js
@@ -1,35 +1,47 @@
 import React from 'react';
 
-export default function Product({
-  data,
-  isActive,
-  handleSelect,
-  removeElement,
-}) {
-  const handleClick = () => {
-    handleSelect();
+class Product extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.state = { isActive: false };
+    this.handleClick = this.props.handleSelect.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  handleClick = () => {
+    this.props.handleSelect();
   };
 
-  const handleDelete = (event) => {
+  handleDelete = (event) => {
     event.stopPropagation();
-    removeElement();
+    this.props.removeElement();
   };
-
-  return (
-    <tr
-      onClick={handleClick}
-      className={isActive ? 'select-color' : 'primary-color'}
-      key={data.id}
-    >
-      <td>{data.itemName}</td>
-      <td>{data.itemPrice}</td>
-      <td>{<img src={data.itemImageURL} alt={data.itemImageAlt} />}</td>
-      <td>{data.itemQuantity}</td>
-      <td>
-        <button className='delete-button' onClick={handleDelete}>
-          Delete
-        </button>
-      </td>
-    </tr>
-  );
+  render() {
+    return (
+      <tr
+        onClick={this.props.handleSelect}
+        className={this.state.isActive ? 'select-color' : 'primary-color'}
+        key={this.props.data.id}
+      >
+        <td>{this.props.data.itemName}</td>
+        <td>{this.props.data.itemPrice}</td>
+        <td>
+          {
+            <img
+              src={this.props.data.itemImageURL}
+              alt={this.props.data.itemImageAlt}
+            />
+          }
+        </td>
+        <td>{this.props.data.itemQuantity}</td>
+        <td>
+          <button className='delete-button' onClick={this.props.handleDelete}>
+            Delete
+          </button>
+        </td>
+      </tr>
+    );
+  }
 }
+export default Product;
