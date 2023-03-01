69bbe07e6297f003178789e1ff90ca3f040b982b
Dzmitry Halavach <kotik1@protonmail.com>
2023-03-01 11:19:50 +0300

    feat: validation of individual fields with error messages

--- ishop3/src/components/EditForm.js
+++ ishop3/src/components/EditForm.js
@@ -5,6 +5,9 @@ class EditForm extends React.Component {
     this.props = props;
     this.state = {
       errorMessage: '',
+      errorsName: false,
+      errorQuantity: false,
+      errorPrice: false,
       itemName: '',
       itemPrice: '',
       itemQuantity: '',
@@ -13,16 +16,44 @@ class EditForm extends React.Component {
     this.handleCanceol = this.handleCancel.bind(this);
     this.validateInput = this.validateInput.bind(this);
     this.setErrorMessage = this.setErrorMessage.bind(this);
+    this.validateName = this.validateName.bind(this);
+    this.validatePrice = this.validatePrice.bind(this);
+    this.validateQuantity = this.validateQuantity.bind(this);
   }
+
+  validateName = () => {
+    if (!this.state?.itemName) this.setState({ errorName: true });
+    else if (
+      this.state.itemName.length >= 3 &&
+      this.state.itemName.length <= 100
+    ) {
+      this.setState({ errorName: false });
+    } else this.setState({ errorName: true });
+  };
+
+  validatePrice = () => {
+    if (!this.state?.itemPrice) this.setState({ errorPrice: true });
+    else if (this.state.itemPrice > 0) this.setState({ errorPrice: false });
+    else this.setState({ errorPrice: true });
+  };
+
+  validateQuantity = () => {
+    if (!this.state?.itemQuantity) this.setState({ errorQuantity: true });
+    else if (this.state.itemQuantity >= 0)
+      this.setState({ errorQuantity: false });
+    else this.setState({ errorQuantity: true });
+  };
 
   validateInput = () => {
     if (
-      this.state.itemName.length >= 3 &&
-      this.state.itemPrice > 0 &&
-      this.state.itemQuantity >= 0
+      this.state.errorName === false &&
+      this.state.errorPrice === false &&
+      this.state.errorQuantity === false
     ) {
       return true;
-    } else return false;
+    } else {
+      return false;
+    }
   };
 
   setErrorMessage = () => {
@@ -61,7 +92,12 @@ class EditForm extends React.Component {
               onChange={(e) => {
                 this.setState({ itemName: e.target.value });
               }}
+              onBlur={this.validateName}
             />
+            <span className='message-error'>
+              {this.state?.errorName &&
+                'Name must be longer than 3 and shorter than 100 characters!'}
+            </span>
 
             <label htmlFor='price'>Item price:</label>
             <input
@@ -71,7 +107,11 @@ class EditForm extends React.Component {
               onChange={(e) => {
                 this.setState({ itemPrice: e.target.value });
               }}
+              onBlur={this.validatePrice}
             />
+            <span className='message-error'>
+              {this.state?.errorPrice && 'Price must be greater than zero!'}
+            </span>
 
             <label htmlFor='quantity'>Item quantity:</label>
             <input
@@ -81,7 +121,12 @@ class EditForm extends React.Component {
               onChange={(e) => {
                 this.setState({ itemQuantity: e.target.value });
               }}
+              onBlur={this.validateQuantity}
             />
+            <span className='message-error'>
+              {this.state?.errorQuantity &&
+                'Quantity must be greater or equal to zero!'}
+            </span>
           </div>
           <button type='submit' className='edit-form-save button-small'>
             Save
