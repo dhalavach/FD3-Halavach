07c797fe5022a097efbd033d5b01af7b0f7ceaff
Dzmitry Halavach <kotik1@protonmail.com>
2023-03-05 22:05:02 +0300

    feat: first version of nesting tags

--- rainbow-frame/src/components/RainbowFrame.js
+++ rainbow-frame/src/components/RainbowFrame.js
@@ -2,30 +2,63 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 
 export default function RainbowFrame(props) {
-  const [colors, setColors] = useState([]);
-  const [frameColor, setFrameColor] = useState('');
+  //const [colors, setColors] = useState([]);
+  // const [frameColor, setFrameColor] = useState('');
+  // let arr = [...props.colors]
+  // useEffect(() => {
 
-  useEffect(() => {
-    const arr = [...props.colors];
-    const color = arr.pop();
-    setColors(arr);
-    setFrameColor(color);
-  }, [props.colors]);
+  //     const arr = [...props.colors];
+  //     const color = arr.pop();
+  //     setColors(arr);
+  //     setFrameColor(color);
+
+  // }, [props.colors]);
+
+  let array=[...props.colors]
+
+
+
+  function helper() {
+    
+    let frameColor = array.pop();
+    //setColors(arr)
+
+    return (
+      <div
+        className='frame'
+        style={{
+          border: 'solid 8px ' + frameColor,
+          padding: '4px',
+        }}
+      >{array.length> 1? helper() : props.children}</div>
+    );
+  }
 
   return (
     <div
       className='frame'
       style={{
-        border: 'solid 8px ' + frameColor,
+        border: 'solid 8px ',
         padding: '4px',
       }}
     >
-      {props.colors.length > 1 ? (
-        <RainbowFrame colors={colors}>{props.children}</RainbowFrame>
-      ) : (
-        props.children
-      )}
+      {array.length >= 1 ? helper() : props.children}
     </div>
   );
-}
 
+  // return arr.length >= 1 ? helper(arr) : <div>{props.children}</div>;
+
+  // <div
+  //   className='frame'
+  //   style={{
+  //     border: 'solid 8px ' + frameColor,
+  //     padding: '4px',
+  //   }}
+  // >
+  //   {arr.length > 1 ? (
+  //     <div colors={colors}>{props.children}</div>
+  //   ) : (
+  //     props.children
+  //   )}
+  // </div>
+}
