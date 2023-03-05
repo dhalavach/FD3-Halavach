95381ab56a05957238b615b61b9fe38d770cdfcb
Dzmitry Halavach <kotik1@protonmail.com>
2023-03-05 22:08:08 +0300

    fix: remove unnecessary div

--- rainbow-frame/src/components/RainbowFrame.js
+++ rainbow-frame/src/components/RainbowFrame.js
@@ -1,27 +1,10 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
 
 export default function RainbowFrame(props) {
-  //const [colors, setColors] = useState([]);
-  // const [frameColor, setFrameColor] = useState('');
-  // let arr = [...props.colors]
-  // useEffect(() => {
-
-  //     const arr = [...props.colors];
-  //     const color = arr.pop();
-  //     setColors(arr);
-  //     setFrameColor(color);
-
-  // }, [props.colors]);
-
-  let array=[...props.colors]
-
-
+  const array = [...props.colors];
 
   function helper() {
-    
-    let frameColor = array.pop();
-    //setColors(arr)
+    const frameColor = array.pop();
 
     return (
       <div
@@ -30,35 +13,11 @@ export default function RainbowFrame(props) {
           border: 'solid 8px ' + frameColor,
           padding: '4px',
         }}
-      >{array.length> 1? helper() : props.children}</div>
+      >
+        {array.length >= 1 ? helper() : props.children}
+      </div>
     );
   }
 
-  return (
-    <div
-      className='frame'
-      style={{
-        border: 'solid 8px ',
-        padding: '4px',
-      }}
-    >
-      {array.length >= 1 ? helper() : props.children}
-    </div>
-  );
-
-  // return arr.length >= 1 ? helper(arr) : <div>{props.children}</div>;
-
-  // <div
-  //   className='frame'
-  //   style={{
-  //     border: 'solid 8px ' + frameColor,
-  //     padding: '4px',
-  //   }}
-  // >
-  //   {arr.length > 1 ? (
-  //     <div colors={colors}>{props.children}</div>
-  //   ) : (
-  //     props.children
-  //   )}
-  // </div>
+  return <div>{array.length >= 1 ? helper() : props.children}</div>;
 }
