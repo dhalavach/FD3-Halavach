8656f961f9563addaf8d36055b13d2f5f037d69d
Dzmitry Halavach <kotik1@protonmail.com>
2023-03-07 22:47:18 +0300

    fix: css styles

--- tree-view/src/App.js
+++ tree-view/src/App.js
@@ -6,16 +6,15 @@ function App() {
   return (
     <div className='Tree-View-App'>
       <h1>Tree View</h1>
-      <div className="panels-container">
-        <section className="tree-panel">
+      <div className='panels-container'>
+        <section className='tree-panel'>
           <h3>Folder Tree</h3>
           <Tree data={[mockData]} />
         </section>
-        <section className="files-panel">
-          <h3>Files</h3> 
-    
-        </section>
 
+        <section className='files-panel'>
+          <h3>Files</h3>
+        </section>
       </div>
     </div>
   );
