26ec3e61d5a17eb1b6a918259b51136d8111c41b
Dzmitry Halavach <kotik1@protonmail.com>
2023-03-26 18:04:54 +0300

    init: project structure

---
+++ react-store/src/App.tsx
@@ -0,0 +1,21 @@
+import './styles.css'
+
+function App() {
+
+  return (
+    <div className="grid-container">
+      <header>
+        <a href='/'>Cart</a>
+      </header>
+      <main>
+        List
+      </main>
+      <footer>
+        2023
+      </footer>
+      
+    </div>
+  )
+}
+
+export default App
