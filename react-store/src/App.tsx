a86e71a39053e2abf6a1c5501b9cc6b2d40ed0dd
Dzmitry Halavach <kotik1@protonmail.com>
2023-03-27 01:17:09 +0300

    feat: filter and order products

--- react-store/src/App.tsx
+++ react-store/src/App.tsx
@@ -1,12 +1,39 @@
 import './styles.css';
 import data from './mockData.json';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Table from './components/Table';
+import Filter from './components/Filter';
 
 function App() {
   const [products, setProducts] = useState(data.products);
   const [sort, setSort] = useState('');
   const [type, setType] = useState('');
+
+  const filter = (e) => {
+    setType(e.target.value);
+  };
+
+  const sortProducts = (e) => {
+    setSort(e.target.value);
+  };
+
+  useEffect(() => {
+    let arr = [...data.products];
+    if (type) {
+      if (type !== 'all') {
+        arr = arr.filter((product) => product.itemType === type);
+      }
+    }
+    if (sort) {
+      if (sort === 'ascending') {
+        arr = arr.sort((a, b) => a.itemPrice - b.itemPrice);
+      } else if (sort === 'descending') {
+        arr = arr.sort((a, b) => b.itemPrice - a.itemPrice);
+      }
+    }
+    setProducts(arr);
+  }, [sort, type]);
+
   return (
     <div className='grid-container'>
       <header>
@@ -15,6 +42,13 @@ function App() {
       <main>
         <div className='content'>
           <div className='main'>
+            <Filter
+              count={products.length}
+              type={type}
+              sort={sort}
+              filter={filter}
+              sortProducts={sortProducts}
+            />
             <Table products={products} />
           </div>
 
---
+++ react-store/src/components/Filter.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function Filter(props: any) {
+  const { count, type, sortProducts, filter, sort } = props;
+  return (
+    <div>
+      <div className='filter-result'>{count} Product(s)</div>
+      <div className='filter-sort'>
+        Order{' '}
+        <select value={sort} onChange={sortProducts}>
+          <option>New</option>
+          <option value='ascending'>Price - ascending</option>
+          <option value='descending'>Price - descending</option>
+        </select>
+      </div>
+
+      <div className='filter-type'>
+        Filter
+        <select value={type} onChange={filter}>
+          <option value='all'>All</option>
+          <option value='motherboard'>Motherboard</option>
+          <option value='CPU'>CPU</option>
+          <option value='GPU'>GPU</option>
+          <option value='PSU'>PSU</option>
+        </select>
+      </div>
+    </div>
+  );
+}
