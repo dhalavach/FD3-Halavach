452389c83b5f63d364ac956adf40d2ad91e14e4f
Dzmitry Halavach <kotik1@protonmail.com>
2023-03-29 13:27:03 +0300

    fix: removed unused dev dependencies and plugins

--- react-store/vite.config.ts
+++ react-store/vite.config.ts
@@ -1,18 +1,16 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
 import babel from 'vite-plugin-babel';
-import commonjs from '@rollup/plugin-commonjs';
-
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), commonjs(), babel({
-    babelConfig: {
-      "targets": "> 0.25%, not dead"
-    }
-  })],
-  
-  
-})
+  plugins: [
+    react(),
+    babel({
+      babelConfig: {
+        targets: '> 0.25%, not dead',
+      },
+    }),
+  ],
+});
