257b3d6ce705f5d07fc944391a9cfc3a2696474e
Dzmitry Halavach <kotik1@protonmail.com>
2023-03-29 12:57:05 +0300

    fix: vite-ts-babel setup

--- react-store/vite.config.ts
+++ react-store/vite.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
-
 import react from '@vitejs/plugin-react'
+import babel from 'vite-plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
+  plugins: [react(), commonjs(), babel({
+    babelConfig: {
+      "targets": "> 0.25%, not dead"
+    }
+  })],
+  
+  
 })
