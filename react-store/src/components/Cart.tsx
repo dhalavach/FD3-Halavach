5c8105441b1b0a03399065018fd75cb7b2b6549d
Dzmitry Halavach <kotik1@protonmail.com>
2024-10-27 18:39:45 +0300

    refactor: Cart component

--- react-store/src/components/Cart.tsx
+++ react-store/src/components/Cart.tsx
@@ -1,114 +1,86 @@
-import { useState, useEffect } from "react";
-import { Fade } from "react-awesome-reveal";
-import { Product, RootState, orderObject } from "../types/Types";
-import { formatMoney } from "../util";
-import CheckoutForm from "./CheckoutForm";
-import { useSelector, useDispatch } from "react-redux";
-import { setCartProducts } from "../slices/cartProductsSlice";
+import { useState, useEffect, useCallback, Fragment } from 'react';
+import { Fade } from 'react-awesome-reveal';
+import { Product, RootState } from '../types/Types';
+import { formatMoney } from '../util';
+import CheckoutForm from './CheckoutForm';
+import { useSelector, useDispatch } from 'react-redux';
+import { setCartProducts } from '../slices/cartProductsSlice';
 
 export default function Cart() {
   const dispatch = useDispatch();
   const cartProducts = useSelector((state: RootState) => state.cartProducts);
 
   const [checkoutFormOpen, setCheckoutFormOpen] = useState(false);
-  const [productToRemove, setProductToRemove] = useState<number | null>();
+  const [productToRemove, setProductToRemove] = useState<number | null>(null);
 
-  // useEffect(() => {
-  //   if (localStorage.getItem('cartProducts')) {
-  //     dispatch(setCartProducts(localStorage.getItem('cartProducts')));
-  //   }
-  // }, []);
-
-  const remove = (product: Product): void => {
-    let newCartProducts = [...cartProducts].filter(
-      (item) => item.id !== product.id
-    );
-    dispatch(setCartProducts(newCartProducts));
-    localStorage.setItem("cartProducts", JSON.stringify(newCartProducts));
-  };
-
+  // Load cart products from localStorage on component mount
   useEffect(() => {
-    dispatch(setCartProducts(cartProducts));
-  }, [productToRemove]);
+    const storedCartProducts = localStorage.getItem('cartProducts');
+    if (storedCartProducts) {
+      dispatch(setCartProducts(JSON.parse(storedCartProducts)));
+    }
+  }, [dispatch]);
 
+  // Set new cart products in localStorage whenever they change
   useEffect(() => {
+    localStorage.setItem('cartProducts', JSON.stringify(cartProducts));
     if (cartProducts.length === 0) setCheckoutFormOpen(false);
   }, [cartProducts]);
 
+  // Removes a product from cart
+  const removeProduct = useCallback(
+    (productId: number) => {
+      const updatedCartProducts = cartProducts.filter((item: Product) => item.id !== productId);
+      dispatch(setCartProducts(updatedCartProducts));
+    },
+    [cartProducts, dispatch]
+  );
+
+  const handleRemoveClick = (product: Product) => {
+    setProductToRemove(product.id);
+    setTimeout(() => {
+      removeProduct(product.id);
+      setProductToRemove(null);
+    }, 1000);
+  };
+
+  // Calculate total cart amount
+  const calculateTotal = () =>
+    cartProducts.reduce((total: number, product: Product) => total + product.count * product.itemPrice, 0);
+
   return (
-    <>
-      <div>
-        {cartProducts.length === 0 ? (
-          <div className="cart cart-header">Empty</div>
-        ) : (
-          <div className="cart cart-header">
-            {cartProducts.length} product(s) in cart
-          </div>
-        )}
+    <Fragment>
+      <div className='cart-header'>
+        {cartProducts.length === 0 ? 'Empty' : `${cartProducts.length} product(s) in cart`}
       </div>
+
       {cartProducts.length > 0 && (
         <div>
-          <div className="cart">
-            <ul className="cart-products">
-              {cartProducts.map((product: Product) => {
-                return (
-                  <Fade key={product.id} direction={"left"} triggerOnce={true}>
-                    <li
-                      className={` ${
-                        product.id == productToRemove ? "removing" : ""
-                      }`}
-                    >
-                      <div>
-                        <img
-                          src={product.itemImage}
-                          alt={product.itemImageAlt}
-                        ></img>
-                      </div>
-                      <div>{product.itemName}</div>
-                      <div className="right">
-                        {`${formatMoney(product.itemPrice)} x  ${
-                          product?.count
-                        }`}
-                        <button
-                          className="cart__button-remove"
-                          onClick={() => {
-                            setProductToRemove(product.id);
-                            setTimeout(() => {
-                              remove(product);
-                              setProductToRemove(null);
-                            }, 1000);
-                          }}
-                        >
-                          Remove
-                        </button>
-                      </div>
-                    </li>
-                  </Fade>
-                );
-              })}
-            </ul>
-          </div>
-          <div className="cart__total">
-            <div>
-              Total:
-              {cartProducts.reduce(
-                (acc: number, curr: Product) =>
-                  acc + Number(curr?.count) * curr.itemPrice,
-                0
-              )}
-            </div>
-          </div>
-          <button
-            className="btn primary"
-            onClick={() => setCheckoutFormOpen(true)}
-          >
+          <ul className='cart-products'>
+            {cartProducts.map((product: Product) => (
+              <Fade key={product.id} direction='left' triggerOnce>
+                <li className={product.id === productToRemove ? 'removing' : ''}>
+                  <img src={product.itemImage} alt={product.itemImageAlt} />
+                  <div>{product.itemName}</div>
+                  <div className='right'>
+                    {`${formatMoney(product.itemPrice)} x ${product.count}`}
+                    <button className='cart__button-remove' onClick={() => handleRemoveClick(product)}>
+                      Remove
+                    </button>
+                  </div>
+                </li>
+              </Fade>
+            ))}
+          </ul>
+
+          <div className='cart__total'>Total: {formatMoney(calculateTotal())}</div>
+          <button className='btn primary' onClick={() => setCheckoutFormOpen(true)}>
             Checkout
           </button>
         </div>
       )}
-      {checkoutFormOpen && (
-        <CheckoutForm setCheckoutFormOpen={setCheckoutFormOpen} />
-      )}
-    </>
+
+      {checkoutFormOpen && <CheckoutForm setCheckoutFormOpen={setCheckoutFormOpen} />}
+    </Fragment>
   );
 }
