cfa9fff8d4e7492909870739d317a6110c903d25
Dzmitry Halavach <kotik1@protonmail.com>
2024-10-28 21:04:41 +0300

    refactor: AddToCartButton component

--- react-store/src/components/AddToCartButton.tsx
+++ react-store/src/components/AddToCartButton.tsx
@@ -1,31 +1,24 @@
-import { useEffect, useState, ReactElement } from "react";
-import { Product, RootState } from "../types/Types";
-import { useSelector } from "react-redux";
+import { useEffect, useState, ReactElement } from 'react';
+import { Product, RootState } from '../types/Types';
+import { useSelector } from 'react-redux';
 
-export default function AddToCartButton(props: {
-  add: (product: Product) => void;
-  product: Product;
-}) {
+export default function AddToCartButton(props: { add: (product: Product) => void; product: Product }): ReactElement {
   const { add, product } = props;
   const [inCart, setInCart] = useState(false);
   const cartProducts = useSelector((state: RootState) => state.cartProducts);
-  const checkIfInCart = (product: Product): boolean => {
-    return cartProducts.findIndex((p: Product) => p.id === product.id) !== -1;
-  };
-
+  const checkIfInCart = (product: Product): boolean =>
+    cartProducts.some((cartProduct: Product) => cartProduct.id === product.id);
   useEffect(() => {
     setInCart(checkIfInCart(product));
-  }, [cartProducts]);
+  }, [cartProducts, product]);
 
   return (
     <button
-      className={`btn ${
-        inCart ? "table__button-in-cart" : "table__button-add"
-      }`}
+      className={`btn ${inCart ? 'table__button-in-cart' : 'table__button-add'}`}
       onClick={() => add(product)}
-      data-testid="add-to-cart-button"
+      data-testid='add-to-cart-button'
     >
-      {inCart ? "Product in Cart" : "Add to Cart"}
+      {inCart ? 'Product in Cart' : 'Add to Cart'}
     </button>
   );
 }
