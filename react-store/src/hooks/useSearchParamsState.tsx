96f9794dd38b2c1459470e9385707543081a3da7
Dzmitry Halavach <kotik1@protonmail.com>
2024-11-05 22:23:18 +0300

    refactor: useSearchParams custom hook

--- react-store/src/hooks/useSearchParamsState.tsx
+++ react-store/src/hooks/useSearchParamsState.tsx
@@ -1,27 +1,22 @@
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams } from 'react-router-dom';
 
 export default function useSearchParamsState(
   searchParamName: string,
   defaultValue: string
-): readonly [
-  searchParamsState: string,
-  setSearchParamsState: (newState: string) => void
-] {
+): readonly [searchParamsState: string, setSearchParamsState: (newState: string) => void] {
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const acquiredSearchParam = searchParams.get(searchParamName);
-  const searchParamsState = acquiredSearchParam ?? defaultValue;
+  // Use the provided searchParamName to extract the relevant parameter value
+  const searchParamsState = searchParams.get(searchParamName) ?? defaultValue;
 
   const setSearchParamsState = (newState: string) => {
-    const next = Object.assign(
-      {},
-      [...searchParams.entries()].reduce(
-        (o, [key, value]) => ({ ...o, [key]: value }),
-        {}
-      ),
-      { [searchParamName]: newState }
-    );
-    setSearchParams(next);
+    // Convert searchParams into an object and update the target search parameter
+    const nextSearchParams = Object.fromEntries(searchParams.entries());
+    nextSearchParams[searchParamName] = newState;
+
+    // Set the updated search parameters
+    setSearchParams(nextSearchParams);
   };
+
   return [searchParamsState, setSearchParamsState];
 }
